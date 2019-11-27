fd9c067d8d60adfdffb1c39d391c112a65f2dc72
Petrus Hoc <hoc081098@gmail.com>
2019-11-27 20:52:14 +0700

    Fix comment

--- lib/util.dart
+++ lib/util.dart
@@ -1,6 +1,6 @@
 extension LastOrNullExt<T> on Iterable<T> {
   ///
-  /// Returns the last element if [list] is not empty, otherwise return null
+  /// Returns the last element if this iterable is not empty, otherwise return null
   ///
   T get lastOrNull => isNotEmpty ? last : null;
 }
