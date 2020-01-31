b9368c1a6d77aa54d7e80a7a6e9fa88bcf68b7fe
Petrus Học <hoc081098@gmail.com>
2020-01-31 21:46:44 +0700

    []

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Admin\Desktop\load_more_flutter_bloc_pattern"
+export "FLUTTER_APPLICATION_PATH=E:\Admin\Documents\Android\load_more_flutter_bloc_pattern"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
